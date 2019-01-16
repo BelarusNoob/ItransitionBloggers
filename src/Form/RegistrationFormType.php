788e4f932e95988bea70fe146de3ab8c7eafcbaf
BelarusNoob <navak.navak@gmail.com>
2019-01-16 03:28:52 +0300

    registration complite
    complite registration front and components

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,24 +4,23 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('fullName')
-            ->add('email')
+            ->add('username', TextType::class)
+            ->add('fullName', TextType::class)
+            ->add('email', EmailType::class)
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
